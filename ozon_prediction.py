9b7713f96c05f5dd40591e467c456ab315efdc04
Alexey Reshetnyak <alexey.reshetnyak@yandex.ru>
2022-03-31 18:29:09 +0300

    Added ozon_prediction.py.

---
+++ ozon_prediction.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+def main():
+    file = './data/PRSA_Data_20130301-20170228/PRSA_Data_Aotizhongxin_20130301\
+                                                                 -20170228.csv'
+    print(file)
+
+if __name__ == '__main__':
+    main()
