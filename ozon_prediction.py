8e95f49e3ceba8e548c5a7f8286bc8a4fb84e114
Alexey Reshetnyak <alexey.reshetnyak@yandex.ru>
2022-03-31 19:01:46 +0300

    Added ozon dencity plot.

--- ozon_prediction.py
+++ ozon_prediction.py
@@ -3,6 +3,8 @@
 
 import pandas as pd
 from IPython.display import display # TODO: is it needed?
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def main():
     #TODO: fit all to 79 cols
@@ -16,6 +18,14 @@ def main():
     print('Are there any duplicated values in data ? : {}\n'.format(data.duplicated().any()))
     print('The total number of null values in each colum:')
     display(data.isnull().sum())
+    data.fillna(value=data.mean(), inplace=True)
+    data.wd.fillna(value='NE', inplace=True)
+    display(data.isnull().any())
+
+    plt.figure(figsize=(12,5))
+    sns.distplot(data['O3'], bins=50)
+    plt.title('Ozon dencity', fontsize=16)
+    plt.show()
 
 if __name__ == '__main__':
     main()
