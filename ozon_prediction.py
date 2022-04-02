1c8b58387a40fccc26038559c9564a0cd19de32b
Alexey Reshetnyak <alexey.reshetnyak@yandex.ru>
2022-04-02 13:39:19 +0300

    Displot changed to histplot.

--- ozon_prediction.py
+++ ozon_prediction.py
@@ -42,7 +42,7 @@ def main():
 
     # Let's do a little visualization
     plt.figure(figsize=(12,5))
-    sns.distplot(data['O3'], bins=50)
+    sns.histplot(data['O3'], bins=100)
     plt.title('Ozon dencity', fontsize=16)
     plt.show()
     #It gives nothing special, data as data.
@@ -53,61 +53,55 @@ def main():
                              'CO', 'O3', 'TEMP', 'PRES',
                              'DEWP', 'RAIN', 'WSPM']]
     sns.heatmap(correlation_data.corr(),cmap=plt.cm.Reds,annot=True)
-    plt.title('Heatmap displaying the correlation matrix of the variables',fontsize=16)
+    plt.title('Heatmap displaying the correlation matrix of the variables',
+               fontsize=16)
     plt.show()
 
-    print("\nWe see that only two pairs of features correlate well.\n\
-          PM 10, PM2.5 and TEMP, DEWP with coefficients 0.87, 0.82,\n\
-          respectively. Not much to care about. Just ignore it.")
-    #input("Press Enter to continue...")
+    """
+    We see that only two pairs of features correlate well.
+    PM 10, PM2.5 and TEMP, DEWP with coefficients 0.87, 0.82,
+    respectively. Not much to care about. Just ignore it.
+    """
 
-
-    print("\nNow we will split data to predictor an outcome featires")
-    #input("Press Enter to continue...")
+    # Now we will split data to predictor an outcome featires
     X = data.drop('O3', axis=1)
     y = data['O3'].to_numpy()
     X_scaled = preprocessing.scale(X)
 
-    print("\nSplint to train and test. It's demanded by the task test data\
-           \n is between 01.06.2016 to 30.11.2016, so split it manually\
-           \n manually see nedeed period it's between 28513 and 32904 rows")
-    #input("Press Enter to continue...")
-
+    """
+    Split to train and test. It's demanded by the task test data
+    is between 01.06.2016 to 30.11.2016, so split it
+    manually see nedeed period it's between 28513 and 32904 rows.
+    """
     d1 = 28513; d2 = 32904
     X_test = X_scaled[d1:d2, :]
     y_test = y[d1:d2]
 
     X_train = np.concatenate((X_scaled[0:d1, :], X_scaled[d2:, :]), axis=0)
     y_train = np.concatenate((y[0:d1], y[d2:]), axis=0)
-
+    
+    # Let's try linear regression
     lin_model = LinearRegression()
     lin_model.fit(X_train,y_train)   # fit the model
-
-    print('Score on train data: {}\n'.format(lin_model.score(X_train,y_train)))
-    print('Score on test data: {}'.format(lin_model.score(X_test,y_test)))
 
     prediction = lin_model.predict(X_test)
     mse = mean_squared_error(y_test, prediction)
     accuracy = r2_score(y_test, prediction)
 
-    print('Mean Squared Error: {}\n'.format(mse))
+    print('Lenear regression Mean Squared Error (MSE): {}\n'.format(mse))
     print('Overall model accuracy: {}'.format(accuracy))
-    print("Accuracy is about 0.6, very poor, let's try another model")
-    #input("Press Enter to continue...")
+    #Accuracy is about 0.6, very poor, let's try another model
 
     decision_tree = DecisionTreeRegressor()
     decision_tree.fit(X_train, y_train)
-    print('Score on train data: {}\n'.format(decision_tree.score(X_train,
-                                                                     y_train)))
-    print('Score on test data: {}\n'.format(decision_tree.score(X_test,
-                                                                      y_test)))
 
     tree_pred = decision_tree.predict(X_test)
     tree_mse = mean_squared_error(y_test, tree_pred)
     tree_accuracy = r2_score(y_test, tree_pred)
 
-    print('Root Mean Squared Error: {}\n'.format(np.sqrt(tree_mse)))
-    print('Overall model accuracy: {}'.format(tree_accuracy))
+    print('Decision tree Root Mean Squared Error: {}\n'.format(np.sqrt(tree_mse)))
+    print('Decision tree model accuracy: {}'.format(tree_accuracy))
+    #Accuracy is about the same as lin. regression, let's try another model
 
     forest = RandomForestRegressor(n_estimators=100,
                                    max_depth=7,
@@ -116,16 +110,13 @@ def main():
                                    min_samples_leaf=3)
 
     forest.fit(X_train, y_train)
-
-    print('Score on train data: {}\n'.format(forest.score(X_train, y_train)))
-    print('Score on test data: {}\n'.format(forest.score(X_test, y_test)))
-
     forest_pred = forest.predict(X_test)
+
     forest_mse = mean_squared_error(y_test, forest_pred)
     forest_accuracy = r2_score(y_test, forest_pred)
 
-    print('Root Mean Squared Error: {}\n'.format(np.sqrt(forest_mse)))
-    print('Overall model accuracy: {}'.format(forest_accuracy))
+    print('Random forest Root Mean Squared Error: {}\n'.format(np.sqrt(forest_mse)))
+    print('Random forest model accuracy: {}'.format(forest_accuracy))
 
     grad_boost = GradientBoostingRegressor(n_estimators=100,
                                            max_depth=7,
@@ -135,11 +126,6 @@ def main():
                                            learning_rate=0.1)
 
     grad_boost.fit(X_train, y_train)
-
-    print('Score on train data: {}\n'.format(grad_boost.score(X_train,
-                                                                     y_train)))
-    print('Score on test data: {}\n'.format(grad_boost.score(X_test,
-                                                                      y_test)))
 
     gboost_pred = grad_boost.predict(X_test)
     gboost_mse = mean_squared_error(y_test, gboost_pred)
